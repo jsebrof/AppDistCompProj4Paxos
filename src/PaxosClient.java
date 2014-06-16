e632d6a1f694e088f1982764e1970f568acdf91d
Jonathan Forbes <jsebrof@gmail.com>
2014-06-15 20:30:13 -0700

    added test println

--- src/PaxosClient.java
+++ src/PaxosClient.java
@@ -38,6 +38,7 @@ public class PaxosClient
 			}
 
 			// Create the reference to the remote object through the rmiregistry
+			System.out.println("TEST");
 			System.out.println("rmi://" + serverIP + ":" + port + "/ThreadsService");
 			PaxosServerInterface server = (PaxosServerInterface)Naming.lookup("rmi://" + serverIP + ":" + port + "/ThreadsService");
 			String returnedString;
